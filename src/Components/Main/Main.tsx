2a341f71ff22240fa6345f41b36c0c7ab6a933b0
Yura <yura5742248@gmail.com>
2021-10-23 15:39:01 +0300

    lesson 18, add page Dialogs

--- src/Components/Main/Main.tsx
+++ src/Components/Main/Main.tsx
@@ -1,19 +1,14 @@
 import React from "react";
-import classes from './Main.module.css';
-import {MyPosts} from "./MyPosts/MyPosts";
+import {Profile} from "./Profile/Profile";
+import {Dialogs} from "./Dialogs/Dialogs";
+import classes from "./Main.module.css";
 
 
 export function Main() {
     return (
-
-        <main className={classes.main}>
-            <div>
-                <img
-                    src="https://media-exp1.licdn.com/dms/image/C4D1BAQGDmALg_8s-Yg/company-background_10000/0/1519799119530?e=2159024400&v=beta&t=4WV9YKR9L3PAEnppWmPPMk5xVnETtWvhZN8NexEzPwM"
-                    alt="img"/>
-            </div>
-            <div>Ava+description</div>
-            <MyPosts/>
-        </main>
+        <div className={classes.main}>
+            <Profile/>
+            {/*<Dialogs/>*/}
+        </div>
     )
 }
