485224042c0385102a1c3529764489027085b099
Yura <yura5742248@gmail.com>
2021-11-25 23:05:01 +0300

    lesson 75, redux-form part 2

--- src/Components/Main/Login/Login.tsx
+++ src/Components/Main/Login/Login.tsx
@@ -1,10 +1,16 @@
 import React from "react";
 import classes from "./Login.module.css";
-import Field, {reduxForm} from "redux-form";
+import  {Field, InjectedFormProps, reduxForm} from "redux-form";
+import {on} from "cluster";
 
-function LoginForm() {
+type FormDataType ={
+    login: string
+    password: string
+    rememberMe: boolean
+}
+const LoginForm: React.FC<InjectedFormProps<FormDataType>> =(props) =>{
     return (
-        <form>
+        <form onSubmit={props.handleSubmit}>
             <div><Field component={'input'} name={'login'} placeholder={'login'}/></div>
             <div><Field component={'input'} name={'password'} placeholder={'password'}/></div>
             <div><Field component={'input'} name={'rememberMe'} type={"checkbox"}/></div>
@@ -14,13 +20,16 @@ function LoginForm() {
         </form>
     )
 }
-const LoginReduxForm = reduxForm({form: 'login'})(LoginForm)
+const LoginReduxForm = reduxForm<FormDataType>({form: 'login'})(LoginForm)
 
 export function Login() {
+   const onSubmit= (formData: FormDataType) => {
+console.log(formData)
+    }
     return (
         <div className={classes.login}>
             <h1>Login</h1>
-            <LoginReduxForm/>
+            <LoginReduxForm onSubmit={onSubmit}/>
         </div>
     )
 }
