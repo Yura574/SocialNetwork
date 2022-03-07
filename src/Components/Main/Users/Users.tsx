3735785b2d0f391723d38e53c1f502d0d21bcf8b
Yura <yura5742248@mail.ru>
2022-03-07 11:33:06 +0300

    lesson 53 class component

--- src/Components/Main/Users/Users.tsx
+++ src/Components/Main/Users/Users.tsx
@@ -3,50 +3,97 @@ import {UsersType} from "./UsersContainer";
 import classes from './User.module.css'
 import axios from 'axios'
 
-export function Users(props: UsersType) {
+export class Users extends React.Component<UsersType> {
 
-    if (props.users.length === 0) {
+    constructor(props: UsersType) {
+        super(props);
+
         axios.get('https://social-network.samuraijs.com/api/1.0/users').then(data => {
 
-                props.setUsers(data.data.items)
+                this.props.setUsers(data.data.items)
             }
         )
     }
 
 
-    return (
-        <div className={classes.body}>
-            <button>Get users</button>
-            <div onClick={() => {
-            }}>Users
-            </div>
-            <div>{props.users.map(u =>
-                <div>
-                    <div className={classes.avatarBlock}>
-                        <div className={classes.userAvatar}>
-                            <img src={u.photos.small
-                                ? u.photos.small
-                                : 'https://proslang.ru/wp-content/uploads/2019/03/avatarka_1-300x300.jpg'}
-                                 alt={'avatar'} className={classes.avatar}/>
-                            <button onClick={() => {
-                                u.followed ? props.follow(u.id) : props.unfollow(u.id)
-                            }}>
-                                {u.followed ? 'follow' : 'unfollow'}
-                            </button>
-                        </div>
-                        <div className={classes.userInfo}>
-                            <div className={classes.userName}>
-                                <div>{u.name}</div>
-                                <div>{u.status}</div>
+    render() {
+        return (
+            <div className={classes.body}>
+                <div>Users</div>
+                <div>{this.props.users.map(u =>
+                    <div>
+                        <div className={classes.avatarBlock}>
+                            <div className={classes.userAvatar}>
+                                <img src={u.photos.small
+                                    ? u.photos.small
+                                    : 'https://proslang.ru/wp-content/uploads/2019/03/avatarka_1-300x300.jpg'}
+                                     alt={'avatar'} className={classes.avatar}/>
+                                <button onClick={() => {
+                                    u.followed ? this.props.follow(u.id) : this.props.unfollow(u.id)
+                                }}>
+                                    {u.followed ? 'follow' : 'unfollow'}
+                                </button>
                             </div>
-                            <div className={classes.userLocation}>
+                            <div className={classes.userInfo}>
+                                <div className={classes.userName}>
+                                    <div>{u.name}</div>
+                                    <div>{u.status}</div>
+                                </div>
+                                <div className={classes.userLocation}>
 
+                                </div>
                             </div>
                         </div>
+
                     </div>
-
-                </div>
-            )}</div>
-        </div>
-    )
+                )}</div>
+            </div>
+        )
+    }
 }
+
+
+// export function Users(props: UsersType) {
+//
+//     if (props.users.length === 0) {
+//         axios.get('https://social-network.samuraijs.com/api/1.0/users').then(data => {
+//
+//                 props.setUsers(data.data.items)
+//             }
+//         )
+//     }
+
+//
+//     return (
+//         <div className={classes.body}>
+//             <div>Users</div>
+//             <div>{props.users.map(u =>
+//                 <div>
+//                     <div className={classes.avatarBlock}>
+//                         <div className={classes.userAvatar}>
+//                             <img src={u.photos.small
+//                                 ? u.photos.small
+//                                 : 'https://proslang.ru/wp-content/uploads/2019/03/avatarka_1-300x300.jpg'}
+//                                  alt={'avatar'} className={classes.avatar}/>
+//                             <button onClick={() => {
+//                                 u.followed ? props.follow(u.id) : props.unfollow(u.id)
+//                             }}>
+//                                 {u.followed ? 'follow' : 'unfollow'}
+//                             </button>
+//                         </div>
+//                         <div className={classes.userInfo}>
+//                             <div className={classes.userName}>
+//                                 <div>{u.name}</div>
+//                                 <div>{u.status}</div>
+//                             </div>
+//                             <div className={classes.userLocation}>
+//
+//                             </div>
+//                         </div>
+//                     </div>
+//
+//                 </div>
+//             )}</div>
+//         </div>
+//     )
+// }
